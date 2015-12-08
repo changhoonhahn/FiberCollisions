03914be79454948c8688a29ce6ea6f1918ddb1e4
changhoonhahn <chh327@nyu.edu>
2015-12-07 19:23:52 -0500

    more git deletes

--- 2pcf/cute_2pcf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./home/users/kww231/src/CUTE-1.2/CUTE/CUTE param_true.txt
-./home/users/kww231/src/CUTE-1.2/CUTE/CUTE param_fced.txt
-./home/users/kww231/src/CUTE-1.2/CUTE/CUTE param_nocoll.txt
